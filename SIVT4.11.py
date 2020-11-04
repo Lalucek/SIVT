573c6c1498bd5f82af9503653d4d3dd7bff743e9
Lalucek <61191070+Lalucek@users.noreply.github.com>
2020-11-04 15:27:29 +0100

    Add files via upload

--- SIVT4.11.py
+++ SIVT4.11.py
@@ -13,3 +13,22 @@ def Valid(text):
         return False
 
 print(Valid(zavorky))
+
+def Valid2(text):
+    zavorky = []
+    for i in text:
+        print(i)
+        if(i == '('):
+            zavorky.append(i)
+        else:
+            if(len(zavorky) == 0):
+                return False;
+            else:
+                zavorky.pop()
+                print(text)
+    if(len(zavorky) == 0):
+        return True
+    else:
+        return False
+
+print(Valid2(zavorky))
