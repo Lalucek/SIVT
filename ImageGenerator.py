1079098a54e03334d0d1d54abdb6381ac81b3a20
Martin Spanel <martin.spanel@newtontech.cz>
2020-11-28 21:50:47 +0100

    consistent matrix dimensions

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -4,15 +4,24 @@ from PIL import Image
 
 stranaX = 512
 stranaY = 512
-sirkaArray = np.linspace(0.0, 1.0, stranaX).reshape((1, stranaX, 1))
-vyskaArray = np.linspace(0.0, 1.0, stranaY).reshape((stranaY, 1, 1))
+sirkaArray = np.tile(
+    np.linspace(0.0, 1.0, stranaX).reshape((1, stranaX, 1)),
+    (stranaY, 1, 3),
+)
+vyskaArray = np.tile(
+    np.linspace(0.0, 1.0, stranaY).reshape((stranaY, 1, 1)),
+    (1, stranaX, 3),
+)
 
 def kolikJeStranaY():
     return vyskaArray
 def kolikJeStranaX():
     return sirkaArray
 def generujBarvu():
-    return np.array([random.random(), random.random(), random.random()]).reshape((1, 1, 3))
+    return np.tile(
+        np.array([random.random(), random.random(), random.random()]).reshape((1, 1, 3)),
+        (stranaY, stranaX, 1),
+    )
 funkceNaVyber = [(0,generujBarvu),(0,kolikJeStranaX),(0,kolikJeStranaY),(1,np.cos),(1,np.sin),(1,np.tan),(2,np.add),(2,np.subtract),(2,np.multiply),(2,np.divide)]
 minimalniHloubka = 2
 maximalniHloubka = 10
@@ -22,6 +31,7 @@ def Generuj(hloubka = 0):
               or (fce[0] == 0 and hloubka >= minimalniHloubka)]
     nArgs,dalsiFunkce = random.choice(funkce)
     args = [Generuj(hloubka + 1) for x in range(nArgs)]
+    print(f'depth: {hloubka} shape: {[x.shape for x in args]}')
     return dalsiFunkce(*args)
 
 obrazek = Generuj()
